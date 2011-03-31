b9af30b6337549e1ee17b3925797059e80e804f1
Jean-Marie Favreau <jm.trivial@gmail.com>
2011-03-31 23:22:21 +0200

    Add copyright and license

--- src/core/Coord.hxx
+++ src/core/Coord.hxx
@@ -1,3 +1,24 @@
+/*****************************************************************************
+    This file is part of Voxigame.
+
+    Copyright (C) 2011 Jean-Marie Favreau <J-Marie.Favreau@u-clermont1.fr>
+                       Université d'Auvergne (France)
+
+    Voxigame is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Voxigame is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+ *****************************************************************************/
+
 #ifndef COORD
 #define COORD
 
