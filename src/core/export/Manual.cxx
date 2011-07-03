0ecbccbdbeaff9e2c3d65d95f59850c99a7000ee
Jean-Marie Favreau <jm.trivial@gmail.com>
2011-07-03 12:10:49 +0200

    Adjust the size of the id according to the number of lines

--- src/core/export/Manual.cxx
+++ src/core/export/Manual.cxx
@@ -922,7 +922,7 @@ QVector<QSharedPointer<QGraphicsScene> > Manual::createStepByStepPages(unsigned 
     drawBoardAndCaption(*(result.back()), point, layout, seenPieces, stepPieces, pgroup, true);
 
     QGraphicsTextItem * text = new QGraphicsTextItem;
-    (*text).setFont(QFont("DejaVu Sans", 7));
+    (*text).setFont(QFont("DejaVu Sans", 14 / nbcolumns));
     (*text).setPlainText(QString("%1").arg(step));
     (*text).setPos(point - QPointF(0., columnmargin / 2));
     QRectF rect = (*text).sceneBoundingRect();
